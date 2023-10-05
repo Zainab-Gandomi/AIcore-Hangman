762e26bb243944c13c4e38421d43bec49366d227
Zainab-Gandomi <zeinabgandomi@yahoo.co.uk>
2023-10-05 20:28:17 +0100

    refactor and optimize current code in mileston_2.py

--- mileston_2.py
+++ mileston_2.py
@@ -1,20 +1,23 @@
 import random
 
-#task 1:  creat list of word
-word_list  = ['apple', 'banana', 'orange', 'strawberry', 'watermelon']
+# Task 1: Create a list of words
+list_of_words = ['apple', 'banana', 'orange', 'strawberry', 'watermelon']
 
-print(word_list)
+print(list_of_words)
 
-#task 2: choose a random word from word_list
-word = random.choice(word_list)
+# Task 2: Choose a random word from the list
+def get_random_word(word_list):
+    return random.choice(word_list)
+
+word = get_random_word(list_of_words)
 
 print(word)
 
-#task 3: create input for letter
-guess = input("pleaase enter a single letter: ")
+# Task 3: Create input for a letter
+user_input_letter = input("Please enter a single letter: ")
 
-# task 4: check the guessing letter length and is alphabetical
-if len(guess) == 1 and guess.isalpha():
+# Task 4: Check the guessing letter length and is alphabetical
+if len(user_input_letter) == 1 and user_input_letter.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
